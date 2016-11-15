bf790c82da4fb5fef7c0cf737b2634de809af9c7
Alexander Kharin <jim.hocens@mail.ru>
2016-11-15 21:35:09 +0500

    tiny socket!

---
+++ tinysocket.cpp
@@ -0,0 +1,520 @@
+
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include "tinysocket.h"
+
+
+#if defined(_WIN32)
+	#include <WinSock2.h>
+	#if defined(_MSC_VER)
+		#pragma comment(lib,"ws2_32.lib") 
+	#endif	
+	typedef int socklen_t;
+#else
+	#include <sys/socket.h>
+	#include <sys/types.h>
+	#include <netinet/in.h>
+	#include <netinet/tcp.h>
+	#include <arpa/inet.h>
+	#include <unistd.h>
+	#include <string.h>
+	#include <errno.h>
+	#define INVALID_SOCKET -1	
+#endif
+
+
+ts::socket_native_error_code get_socket_error_code()
+{
+#if defined(_WIN32)
+	return WSAGetLastError();
+#else
+	return errno;
+#endif
+}
+
+
+
+#include <stdexcept>
+
+const char* socket_errors[] = {
+		"socket is closed",
+		"destination address required",
+		"message size",
+		"protocol type",
+		"protocol option",
+		"protocol not supported",
+		"socket not supported",
+		"operation not supported",
+		"protocol family not supported",
+		"address family not supported",
+		"address already in use",
+		"address not available",
+		"network down",
+		"network unreachable",
+		"network reset" ,
+		"connection aborted" ,
+		"connection reset",
+		"no buffer space available" ,
+		"is connected",
+		"not connected",
+		"shutdown" ,
+		"timed out" ,
+		"connection refused",
+		"host down",
+		"host unreachable"
+	};
+
+
+const char* errormsg_from_native_code(ts::socket_native_error_code code)
+{
+#ifdef _WIN32
+	ts::socket_native_error_code native_code = code - 10038;
+	if (native_code >= 0 && native_code < 27)
+		return socket_errors[native_code];
+#else
+
+	ts::socket_native_error_code native_code = code - 88;
+	if (native_code >= 0 && native_code < 27)
+		return socket_errors[native_code];
+#endif
+	return "socket undefined error (see the error code)";
+}
+
+ts::socket_exception::socket_exception(const char * message, socket_native_error_code code)
+{
+	snprintf(_exceptionMessgeBuffer, sizeof(_exceptionMessgeBuffer),
+		"%s: %s", message, errormsg_from_native_code(code));
+	
+}
+
+const char * ts::socket_exception::what() const throw()
+{
+	return _exceptionMessgeBuffer;
+}
+
+ts::socket_native_error_code ts::socket_exception::formated_error_code() const throw()
+{
+#ifdef _WIN32
+	return _code - 10038;
+#else
+	return _code - 88;
+#endif
+
+}
+
+ts::socket_native_error_code ts::socket_exception::system_error_code() const throw()
+{
+	return _code;
+}
+
+
+
+int native_enum_socket_type(int ttype)
+{
+	switch (ttype)
+	{
+	case ts::stream:
+		return SOCK_STREAM;
+		break;
+	case ts::dgram:
+		return SOCK_DGRAM;
+		break;
+	default:
+		return -1;
+	}
+}
+
+int native_enum_address_famaly(ts::address_famaly ttype)
+{
+	switch (ttype)
+	{
+	case ts::internet_network:
+		return AF_INET;
+		break;
+	case ts::internet_network_ipv6:
+		return AF_INET6;
+		break;
+	default:
+		return -1;
+	}
+}
+
+
+int native_enum_protocol_type(int ttype)
+{
+	switch (ttype)
+	{
+	case ts::tcp:
+		return IPPROTO_TCP;
+		break;
+	case ts::udp:
+		return IPPROTO_UDP;
+		break;
+	default:
+		return -1;
+	}
+}
+
+
+
+int un_native_enum_socket_type(int ttype)
+{
+	switch (ttype)
+	{
+	case SOCK_STREAM:
+		return ts::stream;
+		break;
+	case SOCK_DGRAM:
+		return ts::dgram;
+		break;
+	default:
+		return -1;
+	}
+}
+
+ts::address_famaly un_native_enum_address_famaly(int ttype)
+{
+	switch (ttype)
+	{
+	case AF_INET:
+		return ts::internet_network;
+		break;
+	case AF_INET6:
+		return ts::internet_network_ipv6;
+		break;
+	default:
+		return (ts::address_famaly)-1;
+	}
+}
+
+
+int un_native_enum_protocol_type(int ttype)
+{
+	switch (ttype)
+	{
+	case IPPROTO_TCP:
+		return ts::tcp;
+		break;
+	case IPPROTO_UDP:
+		return ts::udp;
+		break;
+	default:
+		return -1;
+	}
+}
+
+
+
+
+
+ts::port::port(int _Port)
+{
+	_port = htons(_Port);
+}
+
+std::ostream & ts::operator<<(std::ostream & _Stream, const ip_address & _Address)
+{
+	return (_Stream << (inet_ntoa(*(in_addr*)&_Address)));
+}
+
+std::ostream & ts::operator<<(std::ostream & _Stream, const port & _Port)
+{
+	return (_Stream << htons(_Port._port));
+}
+
+ts::ip_address::ip_address(const char * _Address)
+{
+
+	this->_address = inet_addr(_Address);
+}
+
+ts::ip_address::ip_address(uint32_t _NativeHotPost)
+{
+	_address = _NativeHotPost;
+}
+
+ts::ip_address::ip_address(ip4_part _A, ip4_part _B, ip4_part _C, ip4_part _D)
+	: _address(0)
+{
+	 
+	_address |= _D;
+	_address = _address << 8;
+
+	_address |= _C;
+	_address = _address << 8;
+
+	_address |= _B;
+	_address = _address << 8;
+
+	_address |= _A;
+	//_address = htonl(_address);
+}
+
+
+void ts::ip_socket_address::serialaze(void * _Dest) const
+{
+	sockaddr_in* _r = (sockaddr_in*)_Dest;
+	memset(_r, 0, sizeof(sockaddr_in));
+	_r->sin_family = native_enum_address_famaly(get_famaly());
+	_r->sin_port = get_port().native_port();
+	_r->sin_addr.s_addr = get_address().native_address();
+}
+
+void ts::ip_socket_address::deserialaze(const void * _Src)
+{
+	sockaddr_in* _r = (sockaddr_in*)_Src;
+	set_famaly(un_native_enum_address_famaly(_r->sin_family));
+	set_port(htons(_r->sin_port));
+	set_address(ip_address(_r->sin_addr.s_addr));
+
+}
+
+
+#if defined(_WIN32)
+char exception_buffer[2048];
+class ws2data_quard 
+{
+public:
+	ws2data_quard() 
+		: is_init(false)
+	{
+		if (::WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		{
+			sprintf_s(exception_buffer, "wsa startup failed: error code: %d", WSAGetLastError());
+			throw std::runtime_error(exception_buffer);
+		}
+		is_init = true;
+	}
+	~ws2data_quard() 
+	{
+		if (is_init)
+			::WSACleanup();
+	}
+
+	//void cheak() 
+	//{
+	//	if(!is_init)
+	//		throw std::runtime_error("error: wsa not sta...");
+	//}
+
+private:
+	bool is_init;
+	WSADATA wsa;
+} _wsintance;
+
+#endif
+
+std::size_t totalBytesSended = 0;
+std::size_t totalBytesReceived = 0;
+std::size_t ts::socket::get_total_bytes_sended()
+{
+	return totalBytesSended;
+}
+
+std::size_t ts::socket::get_total_bytes_received()
+{
+	return totalBytesReceived;
+}
+
+ts::socket::socket(ts::address_famaly _Famaly, ts::socket_type _SocketTpye, ts::protocol_type _ProtocolType)  throw(socket_exception)
+	: _endpoint(ts::ip_socket_address(ts::ip_address_none, 0))
+{
+	//_wsintance.cheak();
+
+	_fd = ::socket(
+		native_enum_address_famaly(_Famaly), 
+		native_enum_socket_type(_SocketTpye),
+		native_enum_protocol_type(_ProtocolType));
+
+	if (_fd == INVALID_SOCKET) 
+		throw socket_exception("error: of create socket", get_socket_error_code());
+}
+
+ts::socket::socket(socket_native_fd _NativeFd) throw(socket_exception)
+	: _endpoint(ts::ip_socket_address(ts::ip_address_none, 0))
+{
+	_fd = _NativeFd;
+}
+
+ts::socket::~socket()
+{
+	close();
+}
+
+void ts::socket::listen(int _maxconnections) throw(socket_exception)
+{
+	if (::listen(_fd, _maxconnections))
+	{
+		throw socket_exception("error: of listen socket", get_socket_error_code());
+	}
+
+}
+
+void ts::socket::bind(const socket_address & _EndPoint) throw(socket_exception)
+{
+	
+	sockaddr isp;
+
+	_EndPoint.serialaze(&isp);
+
+	if (::bind(_fd, &isp, sizeof(isp)))
+	{
+		//std::cout << WSAGetLastError() << std::endl;
+		throw socket_exception("error: of bind socket", get_socket_error_code());
+	}
+}
+
+ts::socket_native_fd ts::socket::get_native_fd()
+{
+	return _fd;
+}
+
+//void ts::socket::set_option(int _Option)
+//{
+//
+//	
+//}
+//
+//int ts::socket::get_option()
+//{
+//	return 0;
+//}
+
+void ts::socket::set_receive_time_out(ms_time_out mssec) throw(socket_exception)
+{
+	struct timeval tv;
+
+	tv.tv_sec = mssec;  /* 30 Secs Timeout */
+	tv.tv_usec = 0;  // Not init'ing this can cause strange errors
+	
+	if (setsockopt(_fd, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(struct timeval))) 
+		throw socket_exception("error: socket: of set option to", get_socket_error_code());
+}
+
+void ts::socket::set_send_time_out(ms_time_out mssec) throw(socket_exception)
+{
+	struct timeval tv;
+
+	tv.tv_sec = mssec;  /* 30 Secs Timeout */
+	tv.tv_usec = 0;  // Not init'ing this can cause strange errors
+
+	if(setsockopt(_fd, SOL_SOCKET, SO_SNDTIMEO, (char *)&tv, sizeof(struct timeval)))
+		throw socket_exception("error: socket: of set option to", get_socket_error_code());
+}
+
+void ts::socket::connect(const socket_address & _To) throw(socket_exception)
+{
+	sockaddr addr_to; 
+	_To.serialaze(&addr_to);
+	
+	if (::connect(_fd, &addr_to, sizeof(sockaddr))) 
+	{
+		throw socket_exception("error: socket: of connect to", get_socket_error_code());
+	}
+	_endpoint.deserialaze(&_To);
+}
+
+size_t ts::socket::send(const void * _Data, size_t _DataLen) throw(socket_exception)
+{
+	totalBytesSended += _DataLen;
+	int rret = ::send(_fd, (const char*)_Data, _DataLen, 0);
+	if (rret < 0)
+		throw socket_exception("error: socket: of send data", get_socket_error_code());
+	return rret;
+}
+
+size_t ts::socket::receive(void * _Data, size_t _DataLen) throw(socket_exception)
+{
+	totalBytesReceived += _DataLen;
+	int rret = ::recv(_fd, (char*)_Data, _DataLen, 0);
+	if (rret < 0)
+		throw socket_exception("error: socket: of receive data", get_socket_error_code());
+	return rret;
+}
+
+#include <iostream>
+size_t ts::socket::send_to(const void * _Data, size_t _DataLen, const socket_address & _To) throw(socket_exception)
+{
+	totalBytesSended += _DataLen;
+	sockaddr data;
+	_To.serialaze(&data);
+	int rret = ::sendto(_fd, (const char*)_Data, _DataLen, 0, &data, sizeof(sockaddr));
+	if (rret < 0) {
+		//std::cout << WSAGetLastError() << std::endl;
+		throw socket_exception("error: socket: of send data to endpoint", get_socket_error_code());
+	
+	}	
+	return rret;
+}
+
+size_t ts::socket::receive_from(void * _Data, size_t _DataLen, socket_address & _From) throw(socket_exception)
+{
+	totalBytesReceived += _DataLen;
+	socklen_t fromlen = sizeof(sockaddr);
+	sockaddr data;
+	_From.serialaze(&data);
+	
+	int rret = ::recvfrom(_fd, (char*)_Data, _DataLen, 0, &data, &fromlen);
+	if (rret < 0) {
+		//std::cout << WSAGetLastError() << std::endl;
+		throw socket_exception("error: socket: of receive from data from endpoint", get_socket_error_code());
+	
+	}
+	_From.deserialaze(&data);
+	return rret;
+}
+
+ts::socket ts::socket::accept() throw(socket_exception)
+{
+	sockaddr recv;
+	socklen_t len = sizeof(sockaddr);
+	memset(&recv, 0, sizeof(recv));
+	int e = ::accept(_fd, &recv, &len);
+	if (e == INVALID_SOCKET)
+		throw socket_exception("error: of accept socket", get_socket_error_code());
+	_endpoint.deserialaze(&recv);
+	return socket(e);
+
+}
+
+
+
+ts::socket * ts::socket::accept_new() throw(socket_exception)
+{
+	sockaddr recv;
+	socklen_t len = 0;
+	memset(&recv, 0, sizeof(recv));
+	ts::socket_native_fd e = ::accept(_fd, &recv, &len);
+	if (e == INVALID_SOCKET)
+		throw socket_exception("error: of accept socket", get_socket_error_code());
+	_endpoint.deserialaze(&recv);
+	return new socket(e);
+}
+
+void ts::socket::close()
+{
+	if (_fd == 0)
+		return;
+#if defined(_WIN32)
+	::closesocket(_fd);
+#else
+	::close(_fd);
+#endif
+	
+	_fd = 0;
+}
+
+void ts::socket::shutdown(socket_shutdown _O) throw(socket_exception)
+{
+	if(::shutdown(_fd, static_cast<int>(_O)))
+		throw socket_exception("error: of shutdown socket", get_socket_error_code());
+}
+
+ts::ip_socket_address ts::socket::remote_endpoint()
+{
+	return _endpoint;
+}
+
+
+
---
+++ tinysocket.h
@@ -0,0 +1,296 @@
+#ifndef __TINY_SOCKET_H__
+#define __TINY_SOCKET_H__
+
+#include <ostream>
+#include <cstdint>
+#include <exception>
+#include <mutex>
+#include <utility>
+
+namespace ts
+{
+	typedef int64_t socket_native_error_code;
+
+	class socket_exception : public std::exception
+	{
+		char _exceptionMessgeBuffer[2048];
+
+		socket_native_error_code _code;
+
+	public:
+		
+		socket_exception(const char* message, socket_native_error_code code);
+
+		virtual const char* what() const throw();
+
+		socket_native_error_code formated_error_code() const throw();
+
+		socket_native_error_code system_error_code() const throw();
+	};
+
+	typedef uint8_t ip4_part;
+
+	typedef int64_t socket_native_fd;
+
+	class ip_address
+	{
+	public:
+
+		explicit ip_address(ip4_part _A, ip4_part _B, ip4_part _C, ip4_part _D);
+
+		explicit ip_address(const char* _Address);
+
+		explicit ip_address(uint32_t _NativeHotPost);
+
+		ip_address(const ip_address& val)
+			: _address(val._address)
+		{}
+
+		friend std::ostream& operator <<(std::ostream& _Stream, const ip_address& _Address);
+
+		uint32_t native_address() const
+		{
+			return _address;
+		}
+
+		ip_address& operator=(const ip_address& val) 
+		{
+			_address = val._address;
+			return *this;
+		}
+		bool operator !=(const ip_address& of) { return _address != of._address; }
+		bool operator ==(const ip_address& of) { return _address == of._address; }
+
+	private:
+		uint32_t _address;
+	};
+
+	std::ostream& operator <<(std::ostream& _Stream, const ip_address& _Address);
+
+	const ip_address ip_address_any(0, 0, 0, 0);
+
+	const ip_address ip_address_loopback(0x7F, 0, 0, 0);
+
+	const ip_address ip_address_broadcast(0xFF, 0xFF, 0xFF, 0xFF);
+
+	const ip_address ip_address_none(0xFF, 0xFF, 0xFF, 0xFF);
+
+	class port
+	{
+	public:
+		port()
+			: _port(0)
+		{}
+
+		port(const port& val)
+			: _port(val._port)
+		{}
+
+		bool operator !=(const port& of) { return _port != of._port; }
+		bool operator ==(const port& of) { return _port == of._port; }
+
+		port& operator=(const port& val)
+		{
+			_port = val._port;
+			return *this;
+		}
+
+		port(int _Port);
+		
+		inline uint16_t native_port() const
+		{
+			return _port;
+		}
+
+		friend std::ostream& operator <<(std::ostream& _Stream, const port& _Port);
+	private:
+
+		uint16_t _port;
+	};
+
+	std::ostream& operator <<(std::ostream& _Stream, const port& _Port);
+
+	enum address_famaly : int
+	{
+		internet_network,
+		internet_network_ipv6
+	};
+
+	enum socket_type : int
+	{
+		stream,
+		dgram
+	};
+
+	enum protocol_type : int
+	{
+		tcp,
+		udp
+	};
+
+	class socket_address
+	{
+	public:
+		virtual ~socket_address() {}
+
+		socket_address(const socket_address&) = default;
+
+		socket_address& operator=(const socket_address&) = default;
+
+		socket_address()
+			: _famaly(internet_network)
+		{}
+
+		address_famaly get_famaly() const
+		{
+			return _famaly;
+		}
+
+		address_famaly set_famaly(const address_famaly& _Famaly)
+		{
+			return _famaly = _Famaly;
+		}
+
+		virtual void serialaze(void* _Dest) const = 0;
+
+		virtual void deserialaze(const void* _Src) = 0;
+	private:
+
+		address_famaly _famaly;
+	};
+
+	class ip_socket_address : public socket_address
+	{
+	public:
+		~ip_socket_address() {}
+
+		ip_socket_address(const ip_socket_address& val)
+			: _address(val._address),
+			_port(val._port)
+		{}
+
+		ip_socket_address& operator=(const ip_socket_address& val)
+		{
+			_port = val._port;
+			_address = val._address;
+			return *this;
+		}
+
+		bool operator !=(const ip_socket_address& of) 
+		{
+			return
+				(_port != of._port) &&
+				(_address != of._address) &&
+				(get_famaly() != of.get_famaly());
+		
+		}
+		bool operator ==(const ip_socket_address& of) {
+			return
+				(_port == of._port) &&
+				(_address == of._address) &&
+				(get_famaly() == of.get_famaly());
+		}
+
+		ip_socket_address(ip_address _Ip, port _Port)
+			: _address(_Ip), _port(_Port)
+		{}
+
+		port get_port() const
+		{
+			return _port;
+		}
+
+		port set_port(const port& _Port)
+		{
+			return _port = _Port;
+		}
+
+		ip_address get_address() const
+		{
+			return _address;
+		}
+
+		ip_address set_address(const ip_address& _Address)
+		{
+			return _address = _Address;
+		}
+
+		virtual void serialaze(void* _Dest) const;
+
+		virtual void deserialaze(const void* _Src);
+	private:
+
+		port _port;
+
+		ip_address _address;
+	};
+
+	enum class socket_shutdown : int
+	{
+		recive = 0,
+		send = 1,
+		both = 2
+	};
+
+	class socket
+	{
+	public:
+
+		static std::size_t get_total_bytes_sended();
+
+		static std::size_t get_total_bytes_received();
+
+		typedef int ms_time_out;
+
+		explicit socket(ts::address_famaly _Famaly, ts::socket_type _SocketTpye, ts::protocol_type _ProtocolType) throw(socket_exception);
+
+		explicit socket(socket_native_fd _NativeFd) throw(socket_exception);
+
+		~socket();
+
+		void listen(int _MaxConnections) throw(socket_exception);
+
+		void bind(const socket_address& _EndPoint) throw(socket_exception);
+
+		socket_native_fd get_native_fd();
+
+		void set_receive_time_out(ms_time_out _Msec) throw(socket_exception);
+
+		void set_send_time_out(ms_time_out _Msec) throw(socket_exception);
+
+		/*void set_option(int _OptionName, int _OptionLevel, base_option* option);
+
+		base_option* get_option(int _OptionName, int _OptionLevel);*/
+
+		void connect(const socket_address& _To) throw(socket_exception);
+
+		size_t send(const void* _Data, size_t _DataLen) throw(socket_exception);
+
+		size_t receive(void* _Data, size_t _DataLen) throw(socket_exception);
+		
+		size_t send_to(const void* _Data, size_t _DataLen, const socket_address& _To) throw(socket_exception);
+
+		size_t receive_from(void* _Data, size_t _DataLen, socket_address& _From) throw(socket_exception);
+
+		socket accept() throw(socket_exception);
+
+		socket* accept_new() throw(socket_exception);
+
+		void close();
+
+		void shutdown(socket_shutdown _O) throw(socket_exception);
+
+		ts::ip_socket_address remote_endpoint();
+	private:
+		
+		ts::ip_socket_address _endpoint;
+
+		socket_native_fd _fd;
+	};
+
+
+
+
+}
+
+#endif //__TINY_SOCKET_H__
